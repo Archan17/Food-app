f8234b9c9653cdd1ebb7a9d82f2b2efb27f58429
Archan Pandya <pandyaarchan17504@gmail.com>
2023-11-30 21:04:31 +0530

    episode-01

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const heading = React.createElement("h1", { id:"heading" }, "Hello from react");
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    [React.createElement(
+        "div",
+        {id:"child1"},
+        [React.createElement("h1",{},"Hello there"),React.createElement("h1",{},"Hello there")] //We can represent it with an array
+    ),
+    React.createElement(
+        "div",
+        {id:"child2"},
+        [React.createElement("h1",{},"Hello there"),React.createElement("h1",{},"Hello there")] //We can represent it with an array
+    ),]
+)
 
-console.log(heading);
 
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root")); // object
-
-root.render(heading);
+root.render(parent);
