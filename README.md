dc25134150f42447087877bd132786dd8b02c627
Archan Pandya <pandyaarchan17504@gmail.com>
2024-01-10 22:23:40 +0530

    Ignited App

---
+++ README.md
@@ -0,0 +1,16 @@
+# Importance of parcel
+
+- Dev Build
+- Local Server
+- HMR : Hot Module Replacement
+- File Watching algorithm - written in c++
+- Caching : Faster Build
+- Image Optimization
+- Building
+- Compress the file
+- Consistant Hashing
+- Code Splitting
+- Differential Bundling - Support Old Browser
+- Diagnostincs - Good Error handling
+- Https
+- Tree Shaking - Remove Unused Code
--- app.js
+++ app.js
@@ -1,19 +1,71 @@
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    [React.createElement(
-        "div",
-        {id:"child1"},
-        [React.createElement("h1",{},"Hello there"),React.createElement("h1",{},"Hello there")] //We can represent it with an array
-    ),
-    React.createElement(
-        "div",
-        {id:"child2"},
-        [React.createElement("h1",{},"Hello there"),React.createElement("h1",{},"Hello there")] //We can represent it with an array
-    ),]
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+//Header Component
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root")); // object
-root.render(parent);
+//Card Component
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        alt="res-logo"
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/zjlfqldr435qla3bzl7p"
+      />
+      <h3>Jay Bhavani Vadapav</h3>
+      <h4>Vadapav, Mumbai, Asian</h4>
+      <h4>5 Stars</h4>
+      <h4>10 minutes</h4>
+    </div>
+  );
+};
+
+//Body Component
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
