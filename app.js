d61228dcf54cfb51bde2f45115af1da75ab80bb8
Archan Pandya <pandyaarchan17504@gmail.com>
2024-03-06 21:52:09 +0530

    Changes Added

--- app.js
+++ app.js
@@ -1,61 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-//Header Component
-const Header = () => {
-  return (
-    <div className="header">
-      <div className="logo-container">
-        <img
-          className="logo"
-          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
-        />
-      </div>
-      <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-//Card Component
-const RestaurantCard = () => {
-  return (
-    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-      <img
-        alt="res-logo"
-        className="res-logo"
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/zjlfqldr435qla3bzl7p"
-      />
-      <h3>Jay Bhavani Vadapav</h3>
-      <h4>Vadapav, Mumbai, Asian</h4>
-      <h4>5 Stars</h4>
-      <h4>10 minutes</h4>
-    </div>
-  );
-};
-
-//Body Component
-const Body = () => {
-  return (
-    <div className="body">
-      <div className="search">Search</div>
-      <div className="res-container">
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-      </div>
-    </div>
-  );
-};
+import Header from "./src/components/Header";
+import Body from "./src/components/Body";
 
 const AppLayout = () => {
   return (
---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+import RestaurantCard from "./RestaurantCard";
+
+//Body Component
+export const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+export default Body;
